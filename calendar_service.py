668c130ce60be3c08f8bb200572285b7af79d5e8
Matthew Wang <matthewwang0106@gmail.com>
2026-02-15 15:43:22 -0800

    modified env variables

--- calendar_service.py
+++ calendar_service.py
@@ -1,13 +1,22 @@
+import json
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-credentials_path = "./gcal-event-adder-c77a1506daf4.json"
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def get_calendar_service(credentials_path: str, calendar_id: str):
-    credentials = service_account.Credentials.from_service_account_file(
-        credentials_path,
-        scopes=['https://www.googleapis.com/auth/calendar']
-    )
+    creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+    if creds_json:
+        credentials = service_account.Credentials.from_service_account_info(
+            json.loads(creds_json),
+            scopes=SCOPES
+        )
+    else:
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials_path,
+            scopes=SCOPES
+        )
     service = build('calendar', 'v3', credentials=credentials)
     return (service, calendar_id)
 
